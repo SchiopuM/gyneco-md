c27768681bf214171580a6f130bcc33c1c7e1c9f
MihailSchiopu <schiopu.mihail96@gmail.com>
2021-10-01 23:17:21 +0300

    Initialise redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const App = () => <div>Hello world!</div>;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
